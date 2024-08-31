0afb6f9f34dc4f50453de61b4d1b20102ef0d393
Bastiaan Stroosnijder <bas@strootje.com>
2024-08-31 18:29:07 +0200

    proper header image sizing

--- src/components/HeaderHero.tsx
+++ src/components/HeaderHero.tsx
@@ -10,10 +10,10 @@ export const HeaderHero = () => {
     <div class="relative flex min-h-dvh items-center justify-center overflow-hidden">
       <div class="m-14 max-w-[690px] rounded bg-[#d1cfba8a] bg-opacity-80 p-24 font-title text-size-5xl shadow-xl">{t("pages.home.hero.text")}</div>
 
-      <picture class="-z-1 absolute h-full">
+      <picture class="-z-1 absolute top-0 right-0 bottom-0 left-0">
         <source srcset={headerAvif} type="image/avif" />
         <source srcset={headerWebp} type="image/webp" />
-        <img class="h-full object-cover object-c" srcset={headerOrig} alt={t("pages.home.hero.image-alt")} />
+        <img srcset={headerOrig} class="h-full w-full object-cover object-c" alt={t("pages.home.hero.image-alt")} />
       </picture>
     </div>
   );
